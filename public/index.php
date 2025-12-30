caf1b4a422598e8a6c82eb499f05aa4040c9913f
evartanyan1989 <evartanyan1989@gmail.com>
2025-12-30 11:20:10 +0800

    Установил композер, создал бутстрап

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../public/bootstrap.php';
+
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH) ?: '/';
+$method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
+
+echo $method . ': ' . $uri;
---
+++ src/Support/Logger.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+
+
+class Logger
+{
+    private static function writeLog(string $level, string $message, array $context): void
+    {
+        $date = date('Y-m-d');
+        $time = date('H:i:s');
+        $filePath = $_ENV['C_REST_LOGS_DIR'] . "/app-{$date}.log";
+
+        $contextString = !empty($context)
+            ? json_encode($context, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT)
+            : '';
+
+        $logMessage = "[{$time}] {$level}: {$message} {$contextString}\n";
+        file_put_contents($filePath, $logMessage, FILE_APPEND);
+    }
+
+    public static function info(string $message, array $context = []): void
+    {
+        self::writeLog('INFO', $message, $context);
+    }
+    public static function error(string $message, array $context = []): void
+    {
+        self::writeLog('ERROR', $message, $context);
+    }
+}
